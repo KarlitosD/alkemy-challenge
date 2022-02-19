9f0729c10b39b45623ba9048ac08c777299b3477
Carlos <carlosdcampos.04@gmail.com>
2022-02-19 20:30:15 -0300

    feat(api): User model

---
+++ api/src/models/User.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model, UUIDV4 } from "sequelize"
+import db from "../db/connection"
+
+class User extends Model{
+   declare id : string
+   declare name : string
+}
+
+User.init({
+   id:{
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      allowNull:false,
+      primaryKey: true,
+      unique: true
+   },
+   name:{
+      type:DataTypes.STRING,
+      allowNull: false
+   },
+   email:{
+      type:DataTypes.STRING,
+      allowNull:true,
+      unique:true,
+   },
+   password:{
+      type:DataTypes.STRING(1000),
+      allowNull:false
+   }
+},{
+   sequelize:db,
+   tableName:"Users",
+   updatedAt:false
+})
+
+export default User
