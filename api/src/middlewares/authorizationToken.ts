194d5fbc2b09566c03d4107ea60a2638bd4627cc
Carlos <carlosdcampos.04@gmail.com>
2022-02-20 23:03:30 -0300

    feat(api): token validation middleware

---
+++ api/src/middlewares/authorizationToken.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import createError from "../utils/createError"
+import CONFIG from "../config/index"
+
+export default (req: Request, res: Response, next: NextFunction) => {
+   try {
+      const header = req.headers["authorization"]
+      if (!header) throw createError(401, "Header authorization not found")
+      const token = header.split(' ')[1]
+      if (!token) throw createError(401, "Empty token") 
+      const tokenValidated = jwt.verify(token, CONFIG.SECRET)
+      if (!tokenValidated) throw createError(401, "The token is not valid")
+      req.userId = (<any>tokenValidated).id
+      next()
+   } catch (error) {
+      next(error)
+   }
+}
