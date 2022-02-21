3cee4e9b6f9c47163a450557f4ede9f47b078950
Carlos <carlosdcampos.04@gmail.com>
2022-02-21 00:51:07 -0300

    feat(api): operation crud routes

--- api/src/routes/operations.router.ts
+++ api/src/routes/operations.router.ts
@@ -1,23 +1,64 @@
-import { Router, Request ,Response } from "express"
+import { Router, Request, Response } from "express"
 import Operation from "../models/Operation"
+import authorizationToken from "../middlewares/authorizationToken"
+import createError from "../utils/createError"
 
 const router = Router()
 
-router.get("/", async (req: Request, res : Response) => {
-    const user = await Operation.findAll()
-    res.send(user)
+router.get("/:userId", async (req: Request, res: Response, next) => {
+    try {
+        const { params, query } = req
+        const limit = query?.limit as string || 100
+        const operations = await Operation.findAll({
+            attributes: ["id", "concept"],
+            where: { UserId: params.userId },
+            limit: +limit
+        })
+        res.status(200).send(operations)
+    } catch (error) {
+        next(error)
+    }
 })
 
-router.post("/", async (req: Request, res : Response) => {
-    const { body } = req
-   //  console.log(body)    
-   //  const { id, name } = await Operation.create({
-   //      name: body.username
-   //  })
-   //  res.send({
-   //      id,
-   //      name
-   //  })
+router.post("/" ,async (req: Request, res: Response, next) => {
+    try {
+        const { body } = req
+        const { id: UserId, concept, amount, type, category } = body 
+        const opeartion = await Operation.create({
+            concept,
+            typeOperation: type,
+            amount,
+            category,
+            UserId,
+        })
+        res.status(201).send(opeartion)     
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.patch("/:operationId", async (req: Request, res: Response, next) => {
+    try {
+        const { body, params } = req
+        const opeartion = await Operation.findByPk(params.operationId)
+        if(!opeartion) throw createError(404, "Operation no exist")
+        opeartion.update({ ...body })
+        res.status(202).send(opeartion)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.delete("/:operationId", async (req: Request, res: Response, next) => {
+    try {
+        const opeartion = await Operation.findByPk(req.params.operationId)
+        if(!opeartion) throw createError(404, "Operation no exist")
+        opeartion.destroy()
+        res.status(202).send(opeartion)
+    } catch (error) {
+        next(error)        
+    }
 })
 
 export default router
