36ff8225a9fe49c3000d58d7687dfb96c0706d07
Carlos <carlosdcampos.04@gmail.com>
2022-02-20 22:57:26 -0300

    fix(api): types returned for bcrypt functions

--- api/src/utils/brcrypt.ts
+++ api/src/utils/brcrypt.ts
@@ -1,8 +1,8 @@
 
 import bcrypt from 'bcrypt'
 
-const hashPassword = (password:string) => bcrypt.hash(password, 10)
+const hashPassword = (password:string): Promise<string> => bcrypt.hash(password, 10)
 
-const comparePassword = (password:string, passwordHash:string) => bcrypt.compare(password, passwordHash)
+const comparePassword = (password:string, passwordHash:string): Promise<boolean> => bcrypt.compare(password, passwordHash)
     
 export { hashPassword, comparePassword }
