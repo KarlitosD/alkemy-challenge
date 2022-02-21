c300675c7ba37563a0819f6f6b1019ad26602b3c
Carlos <carlosdcampos.04@gmail.com>
2022-02-21 03:40:45 -0300

    fix(api): verify token start with "bearer"

--- api/src/middlewares/authorizationToken.ts
+++ api/src/middlewares/authorizationToken.ts
@@ -6,8 +6,8 @@ import CONFIG from "../config/index"
 export default (req: Request, res: Response, next: NextFunction) => {
    try {
       const header = req.headers["authorization"]
-      if (!header) throw createError(401, "Header authorization not found")
-      const token = header.split(' ')[1]
+      if (!header || !header.toLocaleLowerCase().startsWith("bearer")) throw createError(401, "Header authorization not found")
+      const token = header.substring(7)
       if (!token) throw createError(401, "Empty token") 
       const tokenValidated = jwt.verify(token, CONFIG.SECRET)
       if (!tokenValidated) throw createError(401, "The token is not valid")
