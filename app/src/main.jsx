8d0e424c8834a12fb8dae91c05abc9b2bb33efdd
Carlos <carlosdcampos.04@gmail.com>
2022-02-22 05:54:27 -0300

    feat(app): use TokenProvider in the main

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,9 +4,13 @@ import "virtual:windi.css"
 import "./index.css"
 import App from "./App"
 
+import TokenProvider from "./contexts/TokenContext"
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<TokenProvider>
+			<App />
+		</TokenProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 )
