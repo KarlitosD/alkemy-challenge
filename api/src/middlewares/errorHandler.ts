eb9ef53d54020621dd8c0bea4c3e6d67abc83321
Carlos <carlosdcampos.04@gmail.com>
2022-02-22 05:00:36 -0300

    fix(api): add early return when there is an error

--- api/src/middlewares/errorHandler.ts
+++ api/src/middlewares/errorHandler.ts
@@ -2,7 +2,7 @@
 import { Request, Response, NextFunction } from "express"
 import { HttpError } from "../utils/createError"
 export default (err: HttpError, req: Request, res: Response, next: NextFunction) => {
-	if (!err.isCustomError) res.status(500).send({ message: err.message })
+	if (!err.isCustomError) return res.status(500).send({ message: err.message })
 
 	const { status, message } = err
 	res.status(status).send({
