7b2e38846400158781b908629e426b405a0d7051
Carlos <carlosdcampos.04@gmail.com>
2022-02-26 05:01:50 -0300

    fix(app): remove import tokenProvider

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,14 +4,10 @@ import "virtual:windi.css"
 import "./index.css"
 import App from "./App"
 
-import TokenProvider from "./contexts/TokenContext"
 
 ReactDOM.render(
 	<React.StrictMode>
-		<TokenProvider>
-			<App />
-
-		</TokenProvider>
+		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
 )
