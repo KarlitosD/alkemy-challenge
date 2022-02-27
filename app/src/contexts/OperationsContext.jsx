fdedc5c0d996b9d1d06450dfd8876d3a26fec4de
Carlos <carlosdcampos.04@gmail.com>
2022-02-27 05:30:42 -0300

    feat(app): methods to delete and update operations

--- app/src/contexts/OperationsContext.jsx
+++ app/src/contexts/OperationsContext.jsx
@@ -45,8 +45,36 @@ const OperationsProvider = ({ children }) => {
 		})
 	}
 
+	const deleteOperation = async id => {
+		const operationsFiltered = operations.filter(operation => operation.id !== id)
+		mutate(operationsFiltered, { revalidate: false })
+		await fetcher(`http://localhost:4000/api/operations/${id}`, {
+			method: "DELETE",
+			mode: "cors",
+			headers: {
+				Authorization: token
+			}
+		})
+	}
+
+	const updateOperation = async (id, data) => {
+		const copyOperations = JSON.parse(JSON.stringify(operations))
+		const operationIndex = copyOperations.findIndex(operation => operation.id === id)
+		Object.assign(copyOperations[operationIndex], data)
+		mutate(copyOperations, { revalidate: false })
+		await fetcher(`http://localhost:4000/api/operations/${id}`, {
+			method: "PATCH",
+			mode: "cors",
+			headers: {
+				Authorization: token,
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(data)
+		})
+	}
+
 	return (
-		<OperationsContext.Provider value={{ operations, addOperation, totalAmount }}>
+		<OperationsContext.Provider value={{ operations, addOperation, totalAmount, deleteOperation, updateOperation }}>
 			{children}
 		</OperationsContext.Provider>
 	)
