e67840a1cca3ee71b4a4f1c143b6629045677cc0
Carlos <carlosdcampos.04@gmail.com>
2022-02-27 04:09:11 -0300

    fix(app): remove header and add Switch router

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, Route } from "wouter"
+import { Switch, Route } from "wouter"
 
 import Home from "./pages/Home"
 import Sign from "./pages/Sign"
@@ -7,16 +7,17 @@ import Sign from "./pages/Sign"
 function App() {
 
 	return (
-		<div className="h-screen bg-blue-900 text-white">
-			<header className="border-b-black border-transparent flex border-2 py-2 gap-4 justify-center">
+		<div className="h-screen bg-light-400">
+			{/* <header className="border-b-black border-transparent flex border-2 py-2 gap-4 justify-center">
 				<Link to="/home" className="text-white">Home</Link>
 				<Link to="/sign" className="text-white">Sign</Link>
-			</header>
-			<div className="flex h-full items-center justify-center ">
+			</header> */}
+			<Switch >
 				<Route path="/home" component={Home}></Route>
 				<Route path="/sign" component={Sign}></Route>
-			</div>
-		</div>
+			</Switch>
+
+		</div >
 	)
 }
 
