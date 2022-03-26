a29bd185c5ea56ffc4ed1b9635e25f9d9fd6c7b2
Carlos <carlosdcampos.04@gmail.com>
2022-03-26 05:18:20 -0300

    feat(app): index redirect to home

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,23 +1,18 @@
 import React from "react"
-import { Switch, Route } from "wouter"
+import { Switch, Route, Redirect } from "wouter"
 
 import Home from "./pages/Home"
 import Sign from "./pages/Sign"
 
 function App() {
-
 	return (
 		<div className="h-screen bg-light-400">
-			{/* <header className="border-b-black border-transparent flex border-2 py-2 gap-4 justify-center">
-				<Link to="/home" className="text-white">Home</Link>
-				<Link to="/sign" className="text-white">Sign</Link>
-			</header> */}
-			<Switch >
+			<Switch>
+				<Route path="/" component={() => <Redirect to="/home" />} />
 				<Route path="/home" component={Home}></Route>
 				<Route path="/sign" component={Sign}></Route>
 			</Switch>
-
-		</div >
+		</div>
 	)
 }
 
