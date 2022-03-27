bec79233cbcdd517927d44896abbde5530504d48
Carlos <carlosdcampos.04@gmail.com>
2022-03-27 19:59:27 -0300

    feat(app): add dev dependency for Vite and Windi

--- app/windi.config.js
+++ app/windi.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "windicss/helpers"
 import formsPlugin from "windicss/plugin/forms"
+import scrollbarPlugin from "@windicss/plugin-scrollbar"
 
 export default defineConfig({
 	attributify: true,
-	plugins: [formsPlugin],
+	plugins: [formsPlugin, scrollbarPlugin],
 })
